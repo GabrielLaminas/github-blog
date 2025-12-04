bd301d8d1c3d1441f8a06a196ed8564f04d56979
GabrielLaminas <93532694+GabrielLaminas@users.noreply.github.com>
2025-12-04 20:31:19 -0300

    feat: create post component

---
+++ src/components/Posts/index.tsx
@@ -0,0 +1,30 @@
+import "./_style.scss";
+
+import PostItem from "../PostItem";
+
+export default function Posts() {
+  return (
+    <section className="container posts">
+      <article className="posts__publications">
+        <div className="posts__publications__text">
+          <p>Publicações</p>
+          <span>6 publicações</span>
+        </div>
+        
+        <form className="posts__publications__form">
+          <input type="text" placeholder="Buscar conteúdo" />
+        </form>
+      </article>
+
+      <ul className="posts__list">
+        <li className="posts__list__item">
+          <PostItem />
+        </li>
+
+        <li className="posts__list__item">
+          <PostItem />
+        </li>
+      </ul>
+    </section>
+  );
+}
