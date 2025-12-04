6573ebb1723a0c19e036a2bec7c67d391ba6e08d
GabrielLaminas <93532694+GabrielLaminas@users.noreply.github.com>
2025-12-04 20:33:17 -0300

    feat: create post item to posts

---
+++ src/components/PostItem/index.tsx
@@ -0,0 +1,14 @@
+import "./_style.scss";
+
+export default function PostItem() {
+  return(
+    <a href="#" className="post__card">
+      <div className="post__card__title">
+        <h2>JavaScript data types and data structures</h2>
+        <span>Há 1 dia</span>
+      </div>
+
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat adipisci consequatur, mollitia eligendi unde necessitatibus accusamus blanditiis exercitationem molestias officia voluptates incidunt maiores repellendus distinctio nisi! Rem labore repellat minima.</p>
+    </a>
+  );
+}
