301a0e08b98c5575903e6efe94f142bf6797219d
GabrielLaminas <93532694+GabrielLaminas@users.noreply.github.com>
2025-12-20 14:28:00 -0300

    feat: create search api call

--- src/services/github.ts
+++ src/services/github.ts
@@ -1,10 +1,22 @@
 import api from "./api";
 
-import type { Users } from "../@types/github";
+import type { Users, Issues } from "../@types/github";
 
 export async function getUsers(username: string) {
   try {
     const response = await api.get<Users>(`users/${username}`);
+    
+    if(response.status === 200){
+      return response.data;
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function getSearchIssues(query: string){
+  try {
+    const response = await api.get<Issues>(`search/issues?q=${query}`);
     
     if(response.status === 200){
       return response.data;
