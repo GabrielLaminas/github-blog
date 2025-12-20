9701b53212b19c783f052ac83f15a66c9dbf82f1
GabrielLaminas <93532694+GabrielLaminas@users.noreply.github.com>
2025-12-20 14:27:18 -0300

    feat: add and config date fns

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,11 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import "./styles/global.scss";
 
+import { ptBR } from "date-fns/locale";
+import { setDefaultOptions } from 'date-fns';
+setDefaultOptions({ locale: ptBR });
+
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
