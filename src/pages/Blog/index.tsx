ffa4b3e5fcf89fb161c69e6efa528a9b5a3fff3d
GabrielLaminas <93532694+GabrielLaminas@users.noreply.github.com>
2025-12-21 12:39:40 -0300

    refactor: set no item list search

--- src/pages/Blog/index.tsx
+++ src/pages/Blog/index.tsx
@@ -1,16 +1,29 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import "./_style.scss";
 
-import { getUsers } from "../../services/github";
+import { getSearchIssues, getUsers } from "../../services/github";
 import PostItem from "../../components/PostItem";
 import Profile from "../../components/Profile";
 
-import type { Users } from "../../@types/github";
+import { type Issues, type Users } from "../../@types/github";
 
 const USERNAME = "GabrielLaminas";
 
 export default function Blog() {
   const [user, setUser] = useState<Users>({} as Users);
+  const [issues, setIssues] = useState<Issues>({} as Issues);
+  const [query, setQuery] = useState("");
+
+  function handleQuerySearch(value: string){
+    setQuery(value);
+  }
+
+  const fetchSearchIssues = useCallback(async() => {
+    const data = await getSearchIssues(query);
+    if(data && data.total_count){
+      setIssues(data);
+    }
+  }, [query])
 
   useEffect(() => {
     async function fetchUsers(){
@@ -19,6 +32,27 @@ export default function Blog() {
     }
     fetchUsers();
   }, []);
+
+  useEffect(() => {
+    async function fetchIssues(){
+      const response = await getSearchIssues(`repo:${USERNAME}/github-blog`);
+      if(response) setIssues(response);
+    }
+    fetchIssues()
+  }, [])
+
+  useEffect(() => {
+    if (!query) {
+      return;
+    }
+
+    const timeoutId = setTimeout(() => {
+      fetchSearchIssues();
+    }, 1000);
+
+    return () => clearTimeout(timeoutId);
+       
+  }, [query, fetchSearchIssues]);
 
   if(!user) return <p>Carregando..</p>
 
@@ -29,22 +63,28 @@ export default function Blog() {
       <article className="blog__publications">
         <div className="blog__publications__text">
           <p>Publicações</p>
-          <span>6 publicações</span>
+          <span>{issues.total_count ?? 0} publicações</span>
         </div>
 
         <form className="blog__publications__form">
-          <input type="text" placeholder="Buscar conteúdo" />
+          <input 
+            type="search" 
+            placeholder="Buscar conteúdo" 
+            value={query}
+            onChange={({ target }) => handleQuerySearch(target.value)}
+          />
         </form>
       </article>
 
       <ul className="blog__list">
-        <li className="blog__list__item">
-          <PostItem link={`post/123`} />
-        </li>
-
-        <li className="blog__list__item">
-          <PostItem link={`post/1234`}  />
-        </li>
+        {issues && issues.items && issues.items.length > 0 ? (
+          issues.items.map((issue) => (
+          <li key={issue.id}>
+            <PostItem data={issue} />
+          </li>
+        ))) : (
+          <li className="no-item">Não há itens</li>
+        )}
       </ul>
     </section>
   );
